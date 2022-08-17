be2bd390b3b28bb62d6613990588b7310c75e9c6
MalcolmDahling <mac.dahling@gmail.com>
2022-08-17 11:24:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -31,10 +31,8 @@ io.on('connection', function(socket){
     socket.on('join', function(room){
         socket.join(room);
         console.log('User connected to room: ' + room);
-        
-        // let openRooms = Array.from(io.sockets.adapter.rooms);
-        // console.log( openRooms );
-        // io.emit('open rooms', );
+
+        console.log(getActiveRooms(io));
     });
 
 
@@ -54,6 +52,20 @@ io.on('connection', function(socket){
     });
 
 });
+
+
+function getActiveRooms(io) {
+    // Convert map into 2D list:
+    // ==> [['4ziBKG9XFS06NdtVAAAH', Set(1)], ['room1', Set(2)], ...]
+    const arr = Array.from(io.sockets.adapter.rooms);
+    // Filter rooms whose name exist in set:
+    // ==> [['room1', Set(2)], ['room2', Set(2)]]
+    const filtered = arr.filter(room => !room[1].has(room[0]))
+    // Return only the room name: 
+    // ==> ['room1', 'room2']
+    const res = filtered.map(i => i[0]);
+    return res;
+}
 
 
 module.exports = {app: app, server: server};
